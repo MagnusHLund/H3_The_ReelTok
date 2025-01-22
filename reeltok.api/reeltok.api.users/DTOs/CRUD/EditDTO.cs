8998921a75b3e8986bfc872654120fe4cda0fc7e
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-01-22 14:09:30 +0100

    User structure is more or less completed

---
+++ reeltok.api/reeltok.api.users/DTOs/CRUD/EditDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reeltok.api.users.DTOs.CRUD
+{
+    public class EditDTO
+    {
+        
+    }
+}
---
+++ reeltok.api/reeltok.api.users/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using reeltok.api.users.Interfaces;
+
+namespace reeltok.api.users.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+    }
+}
