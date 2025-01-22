bafd6feb43e89a09f0e473885b77096d9d1c9b9e
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-01-22 14:38:39 +0100

    Video structure is more or less completed

---
+++ reeltok.api/reeltok.api.videos/Repositories/VideoRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using reeltok.api.videos.Interfaces;
+
+namespace reeltok.api.videos.Repositories
+{
+    public class VideoRepository : IVideoRepository
+    {
+        
+    }
+}
