5a694e7b8c85485d5e5be6aec80975100a44bee6
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-01-22 11:08:20 +0100

    Sturcture is more or less ready 😄

---
+++ reeltok.api/reeltok.api.users/Services/UserService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using reeltok.api.users.Interfaces;
+namespace reeltok.api.users.Services
+{
+    public class UserService : IUserService
+    {
+
+    }
+}
