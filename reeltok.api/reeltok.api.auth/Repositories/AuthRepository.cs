e99226ee5120ace09b26f4edc492d75898cca67b
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-01-22 14:03:48 +0100

    Auth structure is more or less completed

---
+++ reeltok.api/reeltok.api.auth/Repositories/AuthRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using reeltok.api.auth.Interfaces;
+
+namespace reeltok.api.auth.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        
+    }
+}
---
+++ reeltok.api/reeltok.api.auth/Services/AuthService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using reeltok.api.auth.Interfaces;
+
+namespace reeltok.api.auth.Services
+{
+    public class AuthService : IAuthService
+    {
+
+    }
+}
