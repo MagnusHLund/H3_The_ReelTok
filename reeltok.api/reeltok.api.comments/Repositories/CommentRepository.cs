a45cc58bfe7659beef2950bd0049e469889ec85a
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-01-23 09:32:59 +0100

    Structure and Comment is ready

---
+++ reeltok.api/reeltok.api.comments/Repositories/CommentRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommentsService.Interface;
+
+namespace CommentsService.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        
+    }
+}
