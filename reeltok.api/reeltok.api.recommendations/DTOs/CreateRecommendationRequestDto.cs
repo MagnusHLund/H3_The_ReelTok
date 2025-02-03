2e92283b77b55071ff5f46f55134a1d0e2279cd8
MarcusL00 <marcuswind123@gmail.com>
2025-02-03 09:59:32 +0100

    removed private set

--- reeltok.api/reeltok.api.recommendations/DTOs/CreateRecommendationRequestDto.cs
+++ reeltok.api/reeltok.api.recommendations/DTOs/CreateRecommendationRequestDto.cs
@@ -4,10 +4,9 @@ namespace reeltok.api.recommendations.DTOs
 {
     internal class CreateRecommendationRequestDto
     {
+        internal string Category { get; set; }
         [Required]
-        internal string Category { get; private set; }
-        [Required]
-        internal Guid UserId { get; private set; }
+        internal Guid UserId { get; set; }
 
         internal CreateRecommendationRequestDto(string category, Guid userId)
         {
