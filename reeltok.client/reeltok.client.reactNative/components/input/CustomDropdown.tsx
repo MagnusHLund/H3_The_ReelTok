233b838c7b50398d627b7b42eba9ef5c4ff9921b
MarcusL00 <marcuswind123@gmail.com>
2025-02-10 15:27:53 +0100

    added

--- reeltok.client/reeltok.client.reactNative/components/input/CustomDropdown.tsx
+++ reeltok.client/reeltok.client.reactNative/components/input/CustomDropdown.tsx
@@ -1,5 +1,35 @@
-// The dropdown does not need much. Its a basic drop down, following the app standard design.
+import DropDownPicker from 'react-native-dropdown-picker';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 
-const CustomDropdown: React.FC = ({}) => {};
 
-export default CustomDropdown
+interface DropDownProps {
+  label: string;
+  categories: { label: string; value: string }[];
+  width?: number;
+  height?: number;
+}
+
+const CustomDropdown: React.FC<DropDownProps> = ({ label, categories, width, height }) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<string | null>(null);
+
+  return (
+    <>
+      <Text>{label}</Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={categories}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={() => {}}
+        placeholder='Select a category'
+        style={{ width: width, height: height,borderRadius: 10 }}
+        
+      />
+    </>
+  );
+};
+
+export default CustomDropdown;
