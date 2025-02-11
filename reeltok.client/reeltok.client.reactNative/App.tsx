60993c404f678bb9f0bed61904499396edcf282f
MarcusL00 <118048523+MarcusL00@users.noreply.github.com>
2025-02-11 14:29:55 +0100

    feat(client.reactNative): :sparkles: added a new component
    custom text input

--- reeltok.client/reeltok.client.reactNative/App.tsx
+++ reeltok.client/reeltok.client.reactNative/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import CustomTextInput from "./components/input/CustomTextInput";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <CustomTextInput placeholder="test" email />
     </View>
   );
 }
