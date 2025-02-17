9b387abf26c7a5dd399821137a5699c47e8100d3
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-02-17 12:47:01 +0100

    feat(api.comments): :art: Entity and value objects have been instantiated and are now primed for integration within the domain model

--- reeltok.api/reeltok.api.comments/Entities/Comment.cs
+++ reeltok.api/reeltok.api.comments/Entities/Comment.cs
@@ -1,12 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using reeltok.api.comments.ValueObjects;
 
 namespace CommentsService.Entities
 {
     public class Comment
     {
-        
+        [Key]
+        public int CommentId { get; private set; }
+
+        [Required]
+        public CommentDetails CommentDetails { get; set; }
+
+        public Comment(CommentDetails commentDetails)
+        {
+            CommentDetails = commentDetails;
+        }
+
+        private Comment()
+        {
+        }
     }
 }
--- reeltok.api/reeltok.api.comments/ValueObjects/CommentDetails.cs
+++ reeltok.api/reeltok.api.comments/ValueObjects/CommentDetails.cs
@@ -1,12 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace reeltok.api.comments.ValueObjects
 {
     public class CommentDetails
     {
-        
+        [Required]
+        public Guid UserId { get; private set; } = Guid.Empty;
+
+        [Required]
+        public Guid VideoId { get; private set; } = Guid.Empty;
+
+        [Required]
+        public string Message { get; private set; } = string.Empty;
+
+        [Required]
+        public uint CreatedAt { get; private set; } = 0;
+
+        public CommentDetails(Guid userId, Guid videoId, string message, uint createdAt)
+        {
+            UserId = userId;
+            VideoId = videoId;
+            Message = message;
+            CreatedAt = createdAt;
+        }
+
+        private CommentDetails()
+        {
+        }
+
     }
 }
