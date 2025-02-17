ced1c4ee03ad8e4f080c365294df34c58accd017
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-02-17 12:31:50 +0100

    feat(api.users): :sparkles: Integrated new dependencies into the project ecosystem

--- reeltok.api/reeltok.api.comments/ValueObjects/CommentDetails.cs
+++ reeltok.api/reeltok.api.comments/ValueObjects/CommentDetails.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CommentsService.Entities
+namespace reeltok.api.comments.ValueObjects
 {
-    public class ValueObject
+    public class CommentDetails
     {
         
     }
