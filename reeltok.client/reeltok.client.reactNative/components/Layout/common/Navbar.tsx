1ea314acb68d6029652265eb82e2fc6a6cb75c34
MarcusL00 <marcuswind123@gmail.com>
2025-02-18 08:32:21 +0100

    feat(client.reactNative): :sparkles: media selector

--- reeltok.client/reeltok.client.reactNative/components/Layout/common/Navbar.tsx
+++ reeltok.client/reeltok.client.reactNative/components/Layout/common/Navbar.tsx
@@ -1,16 +1,18 @@
 // The navbar needs to be able to be at the bottom of the screen, in vertical mode, but on the right side of the screen while in horizontal mode.
 // The icons inside the navbar should turn to the correct axis, for the given screen orientation.
 
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import CustomButton from '../../input/CustomButton';
+import MediaSelector from './MediaSelector';
 
 const Navbar: React.FC = () => {
-  const navigation = useNavigation<NativeStackNavigationProp>();
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const [showComponent, setShowComponent] = useState(false);
 
   const styles = StyleSheet.create({
       container: {
@@ -29,9 +31,10 @@ const Navbar: React.FC = () => {
   
   return (
     <>
+    {showComponent && <MediaSelector/>}
       <View style={styles.container}>
-        <CustomButton transparent={true} onPress={() => navigation.navigate('VideoFeed')} > <Ionicons name="play" size={32} color="white" /> </CustomButton>
-        <CustomButton transparent={true} onPress={() => console.log("hello")}> <Ionicons name="add" size={32} color="white" /> </CustomButton>
+        <CustomButton transparent={true} onPress={() => navigation.navigate('VideoFeed')}> <Ionicons name="play" size={32} color="white" /></CustomButton>
+        <CustomButton transparent={true} onPress={() => setShowComponent(!showComponent)}> <Ionicons name="add" size={32} color="white" /> </CustomButton>
         <CustomButton transparent={true} onPress={() => navigation.navigate('Profile')} > <Ionicons name="person-circle-sharp" size={32} color="white" /></CustomButton>
       </View>
     </>
