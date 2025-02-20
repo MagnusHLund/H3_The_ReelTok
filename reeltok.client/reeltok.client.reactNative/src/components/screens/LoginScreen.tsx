5b89ee68ff70048196bb9d4ef39f7d5d7a5b46f0
MarcusL00 <marcuswind123@gmail.com>
2025-02-20 12:48:38 +0100

    style(client.reactNative): :sparkles: fixed issue

--- reeltok.client/reeltok.client.reactNative/src/components/screens/LoginScreen.tsx
+++ reeltok.client/reeltok.client.reactNative/src/components/screens/LoginScreen.tsx
@@ -2,15 +2,25 @@ import { View, StyleSheet, Image } from "react-native";
 import React from "react";
 import CustomTextInput from "../input/CustomTextInput";
 import CustomButton from "../input/CustomButton";
+import { useWindowDimensions } from "react-native";
 
 const LoginScreen = () => {
+
+  const {height, width} = useWindowDimensions();
   const styles = StyleSheet.create({
-    container: {
+    container:{
+      height: height,
+      width: width,
+
+    },
+    inputcontainer: {
       flexDirection: "column",
       marginLeft: "10%",
       top: "20%",
       justifyContent: "space-evenly",
       height: "30%",
+      marginBottom: '-30%',
+      maxHeight: '50%',
     },
     logocontainer: {
       display: "flex",
@@ -18,18 +28,18 @@ const LoginScreen = () => {
     logo: {
       justifyContent: "center",
       width: "90%",
-      height: "59%",
+      height: "70%",
     },
   });
   return (
-    <View>
+    <View styles={styles.container}>
       <View style={styles.logocontainer}>
         <Image
           style={styles.logo}
           source={require("./../../../assets/images/icons/ReelTok_3.png")}
         />
       </View>
-      <View style={styles.container}>
+      <View style={styles.inputcontainer}>
         <CustomTextInput placeholder="Email.." email></CustomTextInput>
         <CustomTextInput placeholder="password.." password></CustomTextInput>
         <CustomButton
