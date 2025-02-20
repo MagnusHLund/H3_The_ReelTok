ce6c9f2507a8c5279f2fae9937586ac40ca18b2c
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-02-20 10:22:27 +0100

    feat(api.recommendations): :art: Integrated Dependency Injection and updated the .csproj file for database connectivity

--- reeltok.api/reeltok.api.recommendations/Program.cs
+++ reeltok.api/reeltok.api.recommendations/Program.cs
@@ -1,20 +1,25 @@
 
+using Microsoft.EntityFrameworkCore;
+using reeltok.api.recommendations.Data;
+
 namespace RecommendationsServiceApi
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
+			builder.Services.AddDbContextFactory<RecommendationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
 			builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
-			var app = builder.Build();
+            WebApplication app = builder.Build();
 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
