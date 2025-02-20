e3340f757db5659b0aec78d87b8756a09a422fa9
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-02-20 19:03:37 +0100

    feat(client.reactNative): :sparkles: Added useOrientation hook

--- reeltok.client/reeltok.client.reactNative/src/hooks/useOrientation.ts
+++ reeltok.client/reeltok.client.reactNative/src/hooks/useOrientation.ts
@@ -1,13 +1,50 @@
-import { DeviceMotionSensor } from './../../node_modules/expo-sensors/src/DeviceMotion';
 import { useState, useEffect } from 'react'
-import { Dimensions } from 'react-native'
+import { DeviceMotion, DeviceMotionMeasurement } from 'expo-sensors'
 
-const useOrientation = () => {
-  const [orientation, setOrientation] = useState('portrait')
+type Orientation = 'up' | 'down' | 'left' | 'right'
 
-  DeviceMotionSensor
+const calculateOrientation = (
+  rotation: DeviceMotionMeasurement['rotation']
+): Orientation | undefined => {
+  const radiansToDegrees = (radians: number) => radians * (180 / Math.PI)
+  const deadZone = 25
+
+  const { beta, gamma } = rotation
+  const betaDegrees = radiansToDegrees(beta)
+  const gammaDegrees = radiansToDegrees(gamma)
+
+  const isInDeadZone = (degrees: number) => degrees > -deadZone && degrees < deadZone
+
+  if (isInDeadZone(betaDegrees)) {
+    if (!isInDeadZone(gammaDegrees)) {
+      return gammaDegrees > deadZone ? 'right' : 'left'
+    }
+  } else if (!isInDeadZone(betaDegrees) && isInDeadZone(gammaDegrees)) {
+    return betaDegrees > deadZone ? 'up' : 'down'
+  }
+
+  return undefined
+}
+
+const UseOrientation = (): Orientation => {
+  const [orientation, setOrientation] = useState<Orientation>('up')
+
+  useEffect(() => {
+    const subscription = DeviceMotion.addListener((data: DeviceMotionMeasurement) => {
+      const newOrientation = calculateOrientation(data.rotation)
+      if (newOrientation !== undefined) {
+        setOrientation(newOrientation)
+      }
+    })
+
+    DeviceMotion.setUpdateInterval(1000)
+
+    return () => {
+      subscription.remove()
+    }
+  }, [])
 
   return orientation
 }
 
-export default useOrientation
+export default UseOrientation
