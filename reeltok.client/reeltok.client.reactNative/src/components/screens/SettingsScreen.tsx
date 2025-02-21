7c6325d3f442e534e2fd261d40bcc0343272d53e
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-02-21 08:31:47 +0100

    feat(client.reactNative): :sparkles: Enhance SettingsScreen with language selection and custom divider

--- reeltok.client/reeltok.client.reactNative/src/components/screens/SettingsScreen.tsx
+++ reeltok.client/reeltok.client.reactNative/src/components/screens/SettingsScreen.tsx
@@ -1,21 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import CustomTextInput from '../input/CustomTextInput'
+import Divider from '../Layout/common/Divider'
+import CustomDropdown from '../input/CustomDropdown'
+import CustomButton from '../input/CustomButton'
 
 const SettingsScreen = () => {
+  const Categories = [
+    { label: 'English', value: 'English' },
+    { label: 'Danish', value: 'Danish' },
+    { label: 'Chinese', value: 'Chinese' },
+    { label: 'Bulgarian', value: 'Bulgarian' },
+  ]
+
   return (
     <View>
       <Text style={styles.ScreenName}>Settings</Text>
-      <View style={{ padding: 20, gap: 10 }}>
-        <Text style={{ fontSize: 25 }}>Change user details</Text>
+      <View style={{ gap: 10, padding: 20 }}>
+        <Divider />
+        <Text style={{ fontSize: 25, fontWeight: '900' }}>Change user details</Text>
         <View>
-          <Text style={{ fontSize: 20 }}>Username</Text>
-          <CustomTextInput placeholder="Username"></CustomTextInput>
+          <Text style={{ fontSize: 20, fontWeight: '600' }}>Username</Text>
+          <View style={{ height: 50 }}>
+            <CustomTextInput placeholder="Username"></CustomTextInput>
+          </View>
         </View>
-        <Text style={{ fontSize: 25 }}>Change user details</Text>
         <View>
-          <Text style={{ fontSize: 20 }}>Username</Text>
-          <CustomTextInput placeholder="Username"></CustomTextInput>
+          <Text style={{ fontSize: 20, fontWeight: '600' }}>Username</Text>
+          <View style={{ height: 50 }}>
+            <CustomTextInput placeholder="Username" />
+          </View>
+        </View>
+        <Divider />
+        <Text style={{ fontSize: 25, fontWeight: '900' }}>Language</Text>
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: '600' }}>Select Language</Text>
+          <CustomDropdown label="" categories={Categories} placeholder="English" />
+        </View>
+        <Divider />
+        <Text style={{ fontSize: 25, fontWeight: '900' }}>Logout</Text>
+        <View>
+          <CustomButton onPress={() => {}} />
         </View>
       </View>
     </View>
