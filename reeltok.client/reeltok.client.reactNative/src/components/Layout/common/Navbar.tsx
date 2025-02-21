e18770e21413528b289c6e8d9c8c9ed09aafb506
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-02-21 09:57:12 +0100

    feat(client.reactNative): :sparkles: FInished implementing useOrientation hook and added it to icon components in the navbar.

--- reeltok.client/reeltok.client.reactNative/src/components/Layout/common/Navbar.tsx
+++ reeltok.client/reeltok.client.reactNative/src/components/Layout/common/Navbar.tsx
@@ -1,39 +1,31 @@
-// The navbar needs to be able to be at the bottom of the screen, in vertical mode, but on the right side of the screen while in horizontal mode.
-// The icons inside the navbar should turn to the correct axis, for the given screen orientation.
-
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import Ionicons from '@expo/vector-icons/Ionicons'
 import CustomButton from '../../input/CustomButton'
 import MediaSelector from './MediaSelector'
-import useOrientation from '../../../hooks/useOrientation'
+import RotatingIcon from './RotatingIcon'
 
 const Navbar: React.FC = () => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>()
   const [displayMediaSelector, setDisplayMediaSelector] = useState(false)
-  const orientation = useOrientation()
 
   const toggleMediaSelectorForVideoUpload = () => {
     setDisplayMediaSelector(!displayMediaSelector)
   }
-  useEffect(() => {
-    console.log(orientation)
-  }, [orientation])
 
   return (
     <>
       {displayMediaSelector && <MediaSelector />}
       <View style={styles.container}>
         <CustomButton transparent={true} onPress={() => navigation.replace('VideoFeed')}>
-          <Ionicons name="play" size={32} color="white" />
+          <RotatingIcon name="play" size={32} color="white" />
         </CustomButton>
-        <CustomButton transparent={true} onPress={() => toggleMediaSelectorForVideoUpload()}>
-          <Ionicons name="add" size={32} color="white" />
+        <CustomButton transparent={true} onPress={toggleMediaSelectorForVideoUpload}>
+          <RotatingIcon name="add" size={32} color="white" />
         </CustomButton>
         <CustomButton transparent={true} onPress={() => navigation.replace('Profile')}>
-          <Ionicons name="person-circle-sharp" size={32} color="white" />
+          <RotatingIcon name="person-circle-sharp" size={32} color="white" />
         </CustomButton>
       </View>
     </>
--- reeltok.client/reeltok.client.reactNative/src/hooks/useOrientation.ts
+++ reeltok.client/reeltok.client.reactNative/src/hooks/useOrientation.ts
@@ -3,6 +3,11 @@ import { DeviceMotion, DeviceMotionMeasurement } from 'expo-sensors'
 
 type Orientation = 'up' | 'down' | 'left' | 'right'
 
+// Due to the app following a TikTok style, the app is mostly fixed in portrait mode.
+// To simplify our styling, we locked the app to be only portrait, within app.json.
+// Because of this, we are unable to use expo-screen-orientation, as it relies on the app not being locked in app.json.
+// Instead we listen to the device's gyroscope and determine the orientation ourselves.
+// This solution does not work well on the emulator, but it works perfectly fine on an actual device.
 const calculateOrientation = (
   rotation: DeviceMotionMeasurement['rotation']
 ): Orientation | undefined => {
