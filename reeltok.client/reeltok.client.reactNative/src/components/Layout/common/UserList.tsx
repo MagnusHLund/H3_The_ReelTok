b776299ada17bfa5d4d7ea4de46ac7b509328b7e
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-02-25 11:56:29 +0100

    feat(client.reactNative): :sparkles: Add UserList and UserCard components for displaying user profiles

---
+++ reeltok.client/reeltok.client.reactNative/src/components/Layout/common/UserList.tsx
@@ -0,0 +1,40 @@
+import CustomButton from '../../input/CustomButton'
+import { StyleSheet, View } from 'react-native'
+import UserCard from '../profile/UserCard'
+import Divider from './Divider'
+import React from 'react'
+
+interface UserListProps {
+  users: {
+    PictureUrl: string
+    ProfileUrl: string
+    Username: string
+    GuidId: string
+  }[]
+}
+
+const UserList: React.FC<UserListProps> = ({ users }) => {
+  return (
+    <View style={{ padding: 10, gap: 10 }}>
+      {users.map((user) => (
+        <>
+          <CustomButton
+            key={user.GuidId}
+            // transparent
+            justifyPosition="space-between"
+            flexDirection="row"
+            widthPercentage={0.95}
+            onPress={() => console.log('User clicked' + user.Username)}
+          >
+            <UserCard user={[{ PictureUrl: user.PictureUrl, Username: user.Username }]} />
+          </CustomButton>
+          <Divider widthPercentage={100} grayColor />
+        </>
+      ))}
+    </View>
+  )
+}
+
+export default UserList
+
+const styles = StyleSheet.create({})
