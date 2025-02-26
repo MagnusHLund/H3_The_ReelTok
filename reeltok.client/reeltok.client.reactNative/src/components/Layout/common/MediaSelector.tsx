7341732f4e3960c7e0744034f32bdeaf7a604f3a
Jorjo6712 <kiro.nicolov@gmail.com>
2025-02-26 13:43:41 +0100

    changed name and formatted files

--- reeltok.client/reeltok.client.reactNative/src/components/Layout/common/MediaSelector.tsx
+++ reeltok.client/reeltok.client.reactNative/src/components/Layout/common/MediaSelector.tsx
@@ -11,7 +11,7 @@ export default function MediaSelector() {
   const handlePickImage = async () => {
     try {
       const selectedImage = await pickImage();
-     
+
     } catch (error) {
       console.error(error.message);
     }
@@ -19,28 +19,30 @@ export default function MediaSelector() {
 
   return (
     <>
-    {showCamera && <CameraSelected/>}
-    <View style={styles.container}>
-      <CustomButton widthPercentage={0.45} onPress={() => { console.log('test');
-        setShowCamera(!showCamera); }}><Entypo name="camera" size={24} color="white" /></CustomButton>
-      <CustomButton widthPercentage={0.45} onPress={() => handlePickImage()}><MaterialIcons name="photo-library" size={24} color="white" /></CustomButton>
-    </View>
+      {showCamera && <CameraSelected />}
+      <View style={styles.container}>
+        <CustomButton widthPercentage={0.45} onPress={() => {
+          console.log('test');
+          setShowCamera(!showCamera);
+        }}><Entypo name="camera" size={24} color="white" /></CustomButton>
+        <CustomButton widthPercentage={0.45} onPress={() => handlePickImage()}><MaterialIcons name="photo-library" size={24} color="white" /></CustomButton>
+      </View>
     </>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flexDirection:'row',
+  container: {
+    flexDirection: 'row',
     justifyContent: 'space-around',
     top: -40,
     backgroundColor: '#262626',
     width: '100%',
     paddingTop: 10,
     borderRadius: 5,
-    marginBottom:5,   
-    paddingLeft:5,
-    paddingRight:5,
-    paddingBottom:15,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 15,
   },
 })
