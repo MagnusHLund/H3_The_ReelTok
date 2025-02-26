c4976c36e86ec21d5bfb67bde6c41e2c65271f6b
Jorjo6712 <kiro.nicolov@gmail.com>
2025-02-26 14:01:02 +0100

    small change

--- reeltok.client/reeltok.client.reactNative/src/components/Layout/common/MediaSelector.tsx
+++ reeltok.client/reeltok.client.reactNative/src/components/Layout/common/MediaSelector.tsx
@@ -4,7 +4,7 @@ import CustomButton from '../../input/CustomButton'
 import Entypo from '@expo/vector-icons/Entypo';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import CameraSelected from './CameraSelected';
-import pickImage from '../../../utils/imagePicker';
+import pickImage from '../../../utils/imagePickerUtils';
 export default function MediaSelector() {
   const [showCamera, setShowCamera] = useState(false);
 
@@ -21,10 +21,7 @@ export default function MediaSelector() {
     <>
       {showCamera && <CameraSelected />}
       <View style={styles.container}>
-        <CustomButton widthPercentage={0.45} onPress={() => {
-          console.log('test');
-          setShowCamera(!showCamera);
-        }}><Entypo name="camera" size={24} color="white" /></CustomButton>
+        <CustomButton widthPercentage={0.45} onPress={() => setShowCamera(!showCamera)}><Entypo name="camera" size={24} color="white" /></CustomButton>
         <CustomButton widthPercentage={0.45} onPress={() => handlePickImage()}><MaterialIcons name="photo-library" size={24} color="white" /></CustomButton>
       </View>
     </>
