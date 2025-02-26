fdd4728909a724aec7033b9152791580087fa28f
Jorjo6712 <kiro.nicolov@gmail.com>
2025-02-26 12:59:59 +0100

    imagePicker works

--- reeltok.client/reeltok.client.reactNative/src/components/Layout/common/MediaSelector.tsx
+++ reeltok.client/reeltok.client.reactNative/src/components/Layout/common/MediaSelector.tsx
@@ -4,28 +4,32 @@ import CustomButton from '../../input/CustomButton'
 import Entypo from '@expo/vector-icons/Entypo';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import CameraSelected from './CameraSelected';
-
-
+import pickImage from '../../../utils/imagePicker';
 export default function MediaSelector() {
   const [showCamera, setShowCamera] = useState(false);
+
+  const handlePickImage = async () => {
+    try {
+      const selectedImage = await pickImage();
+     
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
-    
     <>
-    
-      {showCamera && <CameraSelected/>}
+    {showCamera && <CameraSelected/>}
     <View style={styles.container}>
       <CustomButton widthPercentage={0.45} onPress={() => { console.log('test');
         setShowCamera(!showCamera); }}><Entypo name="camera" size={24} color="white" /></CustomButton>
-      <CustomButton widthPercentage={0.45} onPress={console.log}><MaterialIcons name="photo-library" size={24} color="white" /></CustomButton>
-      
+      <CustomButton widthPercentage={0.45} onPress={() => handlePickImage()}><MaterialIcons name="photo-library" size={24} color="white" /></CustomButton>
     </View>
     </>
   )
 }
 
 const styles = StyleSheet.create({
-
-
   container:{
     flexDirection:'row',
     justifyContent: 'space-around',
@@ -33,13 +37,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#262626',
     width: '100%',
     paddingTop: 10,
-    
     borderRadius: 5,
     marginBottom:5,   
     paddingLeft:5,
     paddingRight:5,
     paddingBottom:15,
-
-
   },
 })
