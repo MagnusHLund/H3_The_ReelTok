a649185ef219940588a4b2275c44bf57b52bd1f9
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-02-27 12:27:09 +0100

    feat: add return types to recommendation service and repository methods

--- reeltok.api/reeltok.api.recommendations/Interfaces/IRecommendationsRepository.cs
+++ reeltok.api/reeltok.api.recommendations/Interfaces/IRecommendationsRepository.cs
@@ -1,10 +1,10 @@
-
-using reeltok.api.recommendations.Entities;
-using reeltok.api.recommendations.Enums;
-
 namespace reeltok.api.recommendations.Interfaces
 {
     public interface IRecommendationsRepository
     {
+        // TODO: Right now most of them are not returning anything, we need to add the return types
+        Task AddRecommendationForUserAsync();
+        Task AddRecommendationForVideoAsync();
+        Task UpdateRecommendationForUserAsync();
     }
 }
