5273a0f9e9d248af3cf6ae94ebc76407370c2f64
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-03-04 12:22:30 +0100

    refactor: simplify RecommendationRepository and RecommendationsService by removing unused methods and updating interface references

--- reeltok.api/reeltok.api.recommendations/Repositories/RecommendationRepository.cs
+++ reeltok.api/reeltok.api.recommendations/Repositories/RecommendationRepository.cs
@@ -1,30 +1,9 @@
-using reeltok.api.recommendations.Data;
-using reeltok.api.recommendations.Interfaces;
+using reeltok.api.recommendations.Interfaces.repositories;
 
 namespace reeltok.api.recommendations.Repositories
 {
     public class RecommendationRepository : IRecommendationsRepository
     {
-        private readonly RecommendationDbContext _Context;
 
-        public RecommendationRepository(RecommendationDbContext recommendationDbContext)
-        {
-            _Context = recommendationDbContext;
-        }
-
-        public Task AddRecommendationForUserAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task AddRecommendationForVideoAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateRecommendationForUserAsync()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
--- reeltok.api/reeltok.api.recommendations/Services/RecommendationsService.cs
+++ reeltok.api/reeltok.api.recommendations/Services/RecommendationsService.cs
@@ -1,32 +1,11 @@
 using reeltok.api.recommendations.Enums;
-using reeltok.api.recommendations.Interfaces;
+using reeltok.api.recommendations.Interfaces.Services;
 
 namespace reeltok.api.recommendations.Services
 {
     public class RecommendationsService : IRecommendationsService
     {
-        private readonly IRecommendationsRepository _recommendationsRepository;
-        public RecommendationsService(IRecommendationsRepository recommendationsRepository)
-        {
-            _recommendationsRepository = recommendationsRepository;
-        }
-
-        public Task AddRecommendationForUserAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task AddRecommendationForVideoAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<RecommendedCategories> GetRecommendedCategoriesAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateRecommendationForUserAsync()
+        public Task<RecommendedCategories> GetAllCategoriesAsync()
         {
             throw new NotImplementedException();
         }
