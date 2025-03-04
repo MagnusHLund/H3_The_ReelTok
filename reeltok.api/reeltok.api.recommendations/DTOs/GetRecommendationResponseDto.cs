8a2a60dd7f95b4516e97738cd3fce1c05f72eac5
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-03-04 13:32:42 +0100

    refactor: update GetRecommendationResponseDto and IRecommendationsService to use string lists instead of RecommendedCategories

--- reeltok.api/reeltok.api.recommendations/DTOs/GetRecommendationResponseDto.cs
+++ reeltok.api/reeltok.api.recommendations/DTOs/GetRecommendationResponseDto.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 using reeltok.api.auth.DTOs;
-using reeltok.api.recommendations.Entities;
-using reeltok.api.recommendations.Enums;
 
 namespace reeltok.api.recommendations.DTOs
 {
     public class GetRecommendationResponseDto : BaseResponseDto
     {
         [Required]
-        public List<RecommendedCategories> Recommendations;
+        public List<string> Recommendations { get; private set; }
 
-        public GetRecommendationResponseDto(List<RecommendedCategories> recommendations, bool success) : base(success)
+        public GetRecommendationResponseDto(List<string> recommendations, bool success) : base(success)
         {
             Recommendations = recommendations;
         }
--- reeltok.api/reeltok.api.recommendations/Interfaces/Services/IRecommendationsService.cs
+++ reeltok.api/reeltok.api.recommendations/Interfaces/Services/IRecommendationsService.cs
@@ -5,7 +5,7 @@ namespace reeltok.api.recommendations.Interfaces.Services
     // TODO: Right now most of them are not returning anything, we need to add the return types
     public interface IRecommendationsService
     {
-        Task<RecommendedCategories> GetAllCategoriesAsync();
+        Task<List<string>> GetAllCategoriesAsync();
 
         /*
         Task GetRecommendedVideosForUserAsync();
