bbac5378a1b38bd346062e2f5b68e969eaeb0694
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-03-11 16:57:38 +0100

    test(api.recommendations): :white_check_mark: Enhance test data factory and add unit tests for VideosService

--- reeltok.api/reeltok.api.recommendations/Tests/Factories/TestDataFactory.cs
+++ reeltok.api/reeltok.api.recommendations/Tests/Factories/TestDataFactory.cs
@@ -6,68 +6,107 @@ namespace reeltok.api.recommendations.Tests.Factories
 {
     public static class TestDataFactory
     {
-        public static CategoryEntity CreateCategoryEntity(CategoryType categoryType = CategoryType.Tech)
+        public static Guid CreateGuid()
+        {
+            return Guid.NewGuid();
+        }
+
+        public static CategoryEntity CreateCategoryEntity(CategoryType categoryType)
         {
             CategoryEntity categoryEntity = new CategoryEntity(categoryType);
 
             return categoryEntity;
         }
 
-        public static CategoryUserInterestEntity CreateCategoryUserInterestEntity(UserEntity user, CategoryEntity category)
+        public static UserEntity CreateUserEntity()
         {
-            // This if is just cause the new instance is giving me yellow line all the time if i don't add this if statement
-            if (category == null)
-            {
-                throw new ArgumentNullException(nameof(category));
-            }
-
-            CategoryUserInterestEntity categoryUserInterestEntity = new CategoryUserInterestEntity(user, category.CategoryId);
-
-            return categoryUserInterestEntity;
-        }
-
-        public static CategoryVideoCategoryEntity CreateCategoryVideoCategoryEntity(VideoEntity video, CategoryEntity category)
-        {
-            // This if is just cause the new instance is giving me yellow line all the time if i don't add this if statement
-            if (category == null)
-            {
-                throw new ArgumentNullException(nameof(category));
-            }
-
-            CategoryVideoCategoryEntity categoryVideoCategoryEntity = new CategoryVideoCategoryEntity(video, category.CategoryId);
-
-            return categoryVideoCategoryEntity;
-        }
-
-        public static UserEntity CreateUserEntity(Guid? userId = null)
-        {
-            UserEntity userEntity = new UserEntity(userId ?? Guid.NewGuid());
+            UserEntity userEntity = new UserEntity(CreateGuid());
 
             return userEntity;
         }
 
-        public static VideoEntity CreateVideoEntity(Guid? videoId = null)
+        public static VideoEntity CreateVideoEntity()
         {
-            VideoEntity videoEntity = new VideoEntity(videoId ?? Guid.NewGuid());
+            VideoEntity videoEntity = new VideoEntity(CreateGuid());
 
             return videoEntity;
         }
 
-        public static WatchedVideoEntity CreateWatchedVideoEntity(
-            Guid? userId = null,
-            Guid? videoId = null,
-            ushort watchCount = 1,
-            uint lastWatchedAt = 0
-        )
+        public static WatchedVideoEntity CreateWatchedVideoEntity()
         {
             WatchedVideoEntity watchedVideoEntity = new WatchedVideoEntity(
-                userId ?? Guid.NewGuid(),
-                videoId ?? Guid.NewGuid(),
-                watchCount,
-                lastWatchedAt
+                CreateGuid(),
+                CreateGuid(),
+                1,
+                (uint) DateTime.UtcNow.Ticks
             );
 
             return watchedVideoEntity;
         }
+
+        public static CategoryUserInterestEntity CreateCategoryUserInterestEntity()
+        {
+            CategoryUserInterestEntity categoryUserInterestEntity = new CategoryUserInterestEntity(CreateUserEntity(), 1);
+
+            return categoryUserInterestEntity;
+        }
+
+        public static CategoryVideoCategoryEntity CreateCategoryVideoCategoryEntity()
+        {
+            CategoryVideoCategoryEntity categoryVideoCategoryEntity = new CategoryVideoCategoryEntity(CreateVideoEntity(), 1);
+
+            return categoryVideoCategoryEntity;
+        }
+
+        public static List<CategoryEntity> CreateCategoryEntities(int count)
+        {
+            List<CategoryEntity> categories = new List<CategoryEntity>();
+            for (int i = 0; i < count; i++)
+            {
+                categories.Add(CreateCategoryEntity((CategoryType) (i % 6)));
+            }
+            return categories;
+        }
+
+        public static List<UserEntity> CreateUserEntities(int count)
+        {
+            List<UserEntity> users = new List<UserEntity>();
+            for (int i = 0; i < count; i++)
+            {
+                users.Add(CreateUserEntity());
+            }
+            return users;
+        }
+
+        public static List<VideoEntity> CreateVideoEntities(int count)
+        {
+            List<VideoEntity> videos = new List<VideoEntity>();
+            for (int i = 0; i < count; i++)
+            {
+                videos.Add(CreateVideoEntity());
+            }
+            return videos;
+        }
+
+        public static List<Guid> CreateVideoIds(int count)
+        {
+            List<Guid> videoIds = new List<Guid>();
+            for (int i = 0; i < count; i++)
+            {
+                videoIds.Add(CreateGuid());
+            }
+            return videoIds;
+        }
+
+        public static List<WatchedVideoEntity> CreateWatchedVideos(int count)
+        {
+            List<WatchedVideoEntity> watchedVideos = new List<WatchedVideoEntity>();
+            for (int i = 0; i < count; i++)
+            {
+                watchedVideos.Add(CreateWatchedVideoEntity());
+            }
+            return watchedVideos;
+        }
+
     }
 }
--- reeltok.api/reeltok.api.recommendations/Tests/Services/VideosServiceTests.cs
+++ reeltok.api/reeltok.api.recommendations/Tests/Services/VideosServiceTests.cs
@@ -1,7 +1,109 @@
+using Moq;
+using Xunit;
+using reeltok.api.recommendations.Enums;
+using reeltok.api.recommendations.Entities;
+using reeltok.api.recommendations.Services;
+using reeltok.api.recommendations.Tests.Factories;
+using reeltok.api.recommendations.Interfaces.Services;
+using reeltok.api.recommendations.Interfaces.Repositories;
+
+
 namespace reeltok.api.recommendations.Tests.Services
 {
     public class VideosServiceTests
     {
+        private readonly Mock<IVideoCategoriesRepository> _mockVideoCategoriesRepository;
+        private readonly Mock<IRecommendationsService> _mockRecommendationsService;
+        private readonly VideosService _videosService;
 
+        public VideosServiceTests()
+        {
+            _mockVideoCategoriesRepository = new Mock<IVideoCategoriesRepository>();
+            _mockRecommendationsService = new Mock<IRecommendationsService>();
+            _videosService = new VideosService(
+                _mockVideoCategoriesRepository.Object,
+                _mockRecommendationsService.Object
+            );
+        }
+
+        #region Success Tests
+
+        [Fact]
+        public async Task GetRecommendedVideosForUsersFeedAsync_WithValidParameters_ReturnsRecommendedVideos()
+        {
+            // Arrange
+            Guid userId = TestDataFactory.CreateGuid();
+            byte amount = 2;
+            List<Guid> recommendedVideoIds = TestDataFactory.CreateVideoIds(amount);
+
+            _mockRecommendationsService.Setup(x => x.GetVideoRecommendationsForUserAsync(userId, amount)).ReturnsAsync(recommendedVideoIds);
+
+            // Act
+            List<Guid> result = await _videosService.GetRecommendedVideosForUsersFeedAsync(userId, amount);
+
+            // Assert
+            Assert.Equal(recommendedVideoIds.Count, result.Count);
+            for (int i = 0; i < recommendedVideoIds.Count; i++)
+            {
+                Assert.Equal(recommendedVideoIds[i], result[i]);
+            }
+        }
+
+        [Fact]
+        public async Task AddVideoCategoryAsync_WithValidParameters_ReturnsSavedCategory()
+        {
+            // Arrange
+            Guid videoId = TestDataFactory.CreateGuid();
+            CategoryType categoryType = CategoryType.Gaming;
+            CategoryEntity categoryEntity = TestDataFactory.CreateCategoryEntity(categoryType);
+
+            // Set up the mock to return a uint when AddVideoCategoryAsync is called
+            _mockVideoCategoriesRepository
+                .Setup(x => x.AddVideoCategoryAsync(It.IsAny<CategoryVideoCategoryEntity>()))
+                .ReturnsAsync((uint) 1);  // Make sure the return value is of type uint
+
+            // Act
+            CategoryType result = await _videosService.AddVideoCategoryAsync(videoId, categoryType);
+
+            // Assert
+            Assert.Equal(categoryType, result);
+        }
+
+
+        #endregion
+
+        #region Failure Tests
+
+        [Fact]
+        public async Task GetRecommendedVideosForUsersFeedAsync_WithInvalidAmount_ReturnsEmptyList()
+        {
+            // Arrange
+            Guid userId = TestDataFactory.CreateGuid();
+            byte amount = 0; // Invalid amount (zero or negative)
+            List<Guid> recommendedVideoIds = new List<Guid>();
+
+            _mockRecommendationsService.Setup(x => x.GetVideoRecommendationsForUserAsync(userId, amount)).ReturnsAsync(recommendedVideoIds);
+
+            // Act
+            List<Guid> result = await _videosService.GetRecommendedVideosForUsersFeedAsync(userId, amount);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task AddVideoCategoryAsync_WithInvalidCategory_ThrowsException()
+        {
+            // Arrange
+            Guid videoId = TestDataFactory.CreateGuid();
+            CategoryType invalidCategory = (CategoryType) 999; // Invalid category value
+
+            _mockVideoCategoriesRepository.Setup(x => x.AddVideoCategoryAsync(It.IsAny<CategoryVideoCategoryEntity>())).ThrowsAsync(new Exception("Invalid category"));
+
+            // Act & Assert
+            await Assert.ThrowsAsync<Exception>(async () => await _videosService.AddVideoCategoryAsync(videoId, invalidCategory));
+        }
+
+        #endregion
     }
 }
