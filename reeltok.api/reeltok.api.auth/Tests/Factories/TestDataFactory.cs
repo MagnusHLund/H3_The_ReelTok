7fe0f6b835360e5ec3467e33b3652b5627b963a1
Shiz <141632242+Shazil0204@users.noreply.github.com>
2025-03-13 12:08:30 +0100

    refactor(tests): :recycle: Add methods to TestDataFactory for generating test data

--- reeltok.api/reeltok.api.auth/Tests/Factories/TestDataFactory.cs
+++ reeltok.api/reeltok.api.auth/Tests/Factories/TestDataFactory.cs
@@ -1,7 +1,21 @@
+using reeltok.api.auth.Entities;
+using reeltok.api.auth.ValueObjects;
+
 namespace reeltok.api.auth.Tests.Factories
 {
     public static class TestDataFactory
     {
+        public static Guid GenerateGuid()
+        {
+            return Guid.NewGuid();
+        }
 
+        public static UserCredentialsEntity GenerateUserCredentialsEntity(Guid userId, string password, string salt)
+        {
+            HashedPasswordDetails hashedPasswordDetails = new HashedPasswordDetails(password, salt);
+            UserCredentialsEntity userCredentialsEntity = new UserCredentialsEntity(userId, hashedPasswordDetails);
+
+            return userCredentialsEntity;
+        }
     }
 }
